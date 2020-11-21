f4a11a62af4c68603a55cb0ab749eb3e219cbc2c
Can Bölük <can.boluk89@gmail.com>
2020-11-21 08:55:22 +0100

    Move out narrow cast.

--- includes/xstd/bitwise.hpp
+++ includes/xstd/bitwise.hpp
@@ -45,28 +45,6 @@ using bitcnt_t = int;
 
 namespace xstd::math
 {
-	// Narrows the given type in a safe manner.
-	//
-	template<Integral T, Integral T2> requires( sizeof( T ) <= sizeof( T2 ) )
-	__forceinline static constexpr T narrow_cast( T2 o )
-	{
-		if constexpr ( std::is_signed_v<T2> ^ std::is_signed_v<T> )
-			dassert( 0 <= o && o <= std::numeric_limits<T>::max() );
-		else
-			dassert( std::numeric_limits<T>::min() <= o && o <= std::numeric_limits<T>::max() );
-
-		return ( T ) o;
-	}
-	template<Integral T, Integral T2> requires( sizeof( T ) <= sizeof( T2 ) )
-	__forceinline static constexpr T narrow_cast_s( T2 o )
-	{
-		return ( T ) std::clamp( 
-			o, 
-			std::min( ( T2 ) std::numeric_limits<T>::min(), std::numeric_limits<T2>::min() ), 
-			( T2 ) std::numeric_limits<T>::max() 
-		);
-	}
-
 	// Extracts the sign bit from the given value.
 	//
 	template<Integral T>
