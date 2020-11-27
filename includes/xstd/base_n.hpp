b1f05e0f9afa0ac3e1732f9f8264215c99e54ad6
Can Bölük <can.boluk89@gmail.com>
2020-11-27 07:20:03 +0100

    Implemented BaseN (e.g. base64) encoding and decoding.

---
+++ includes/xstd/base_n.hpp
@@ -0,0 +1,228 @@
+// Copyright (c) 2020, Can Boluk
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+#pragma once
+#include <string_view>
+#include <vector>
+#include "bitwise.hpp"
+#include "type_helpers.hpp"
+
+namespace xstd::encode
+{
+    namespace impl
+    {
+        // Helper to create a reverse dictionary.
+        //
+        template<size_t N>
+        static constexpr std::array<char, 0x100> reverse_dictionary( const char( &v )[ N ] )
+        {
+            std::array<char, 0x100> res = { -1 };
+            for ( size_t n = 0; n != N; n++ )
+                res[ v[ n ] ] = n;
+            return res;
+        }
+    };
+
+    // Declare a basic dictionary with encoding and decoding traits.
+    //
+    template<size_t N> requires ( N < 0x100 && math::popcnt( N ) == 1 )
+    struct dictionary
+    {
+        // Number of bits encoded per character, e.g. base64 => 6.
+        //
+        static constexpr bitcnt_t _bits_per_char = math::lsb( N );
+
+        // Calculates the group size we encode on each step, e.g. base64 => { 4 out, 3 in }.
+        //
+        static constexpr bitcnt_t _group_size_out = [ ] ()
+        {
+            for ( size_t n = 1; n <= 8; n++ )
+                if ( !( ( n * _bits_per_char ) % 8 ) )
+                    return n;
+        }();
+        static constexpr bitcnt_t _group_size_in = ( _group_size_out * _bits_per_char ) / 8;
+
+        // Bit mask.
+        //
+        static constexpr uint64_t _mask = math::fill( _bits_per_char );
+
+        // Forward and reverse dictionaries.
+        //
+        std::array<char, N + 2> lookup;
+        std::array<char, 0x100> rlookup;
+        constexpr dictionary( const char( &v )[ N + 2 ] ) : lookup( std::to_array( v ) ), rlookup( impl::reverse_dictionary( v ) ) {}
+
+        // Helper methods.
+        //
+        constexpr char encode( char n ) const
+        {
+            dassert( n < N );
+            return lookup[ n ];
+        }
+        constexpr char decode( char n ) const
+        {
+            char r = rlookup[ n ];
+            dassert( 0 <= r && r <= N );
+            return r;
+        }
+        constexpr char fill() const { return lookup[ N ]; }
+        constexpr size_t size() const { return N; }
+        constexpr size_t group_size_in() const { return _group_size_in; }
+        constexpr size_t group_size_out() const { return _group_size_out; }
+        constexpr size_t bits_per_char() const { return _bits_per_char; }
+        constexpr size_t mask() const { return _mask; }
+    };
+    template<size_t N>
+    dictionary( const char( &v )[ N ] )->dictionary<N-2>;
+
+    // Decodes data using a dictionary.
+    //
+    namespace impl
+    {
+        template<size_t N, typename C = char, bool bit_rev = true>
+        static std::vector<uint8_t> rbase_n( std::basic_string_view<C> str, const dictionary<N>& dictionary )
+        {
+            fassert( ( str.length() % dictionary.group_size_out() ) == 0 );
+
+            // Calculate the group count and reserve the output.
+            //
+            std::vector<uint8_t> result;
+            size_t group_count = str.size() / dictionary.group_size_out();
+            result.resize( group_count * dictionary.group_size_in() + 1 );
+            uint8_t* rit = result.data();
+
+            // Read each group:
+            //
+            size_t offset = 0;
+            for ( auto& c : str )
+            {
+                if ( c != dictionary.fill() )
+                {
+                    uint16_t v = dictionary.decode( c );
+                    if constexpr ( bit_rev )
+                    {
+                        v <<= 8 - dictionary.bits_per_char();
+                        v = math::bit_reverse<uint8_t>( v );
+                        v &= dictionary.mask();
+
+                        rit[ 0 ] |= math::bit_reverse<uint8_t>( v << offset );
+                        rit[ 1 ] |= math::bit_reverse<uint8_t>( v >> ( 8 - offset ) );
+                    }
+                    else
+                    {
+                        rit[ 0 ] |= v << offset;
+                        rit[ 1 ] |= v >> ( 8 - offset );
+                    }
+
+                    offset += dictionary.bits_per_char();
+                    rit += offset / 8;
+                    offset %= 8;
+                }
+                else
+                {
+                    rit += offset != 0;
+                    break;
+                }
+            }
+            result.resize( rit - result.data() );
+            return result;
+        }
+    };
+    template<size_t N, String S, bool bit_rev = true>
+    static std::vector<uint8_t> rbase_n( S&& str, const dictionary<N>& dictionary )
+    {
+        return impl::rbase_n( string_view_t<S>{ str }, dictionary );
+    }
+
+    // Encodes data using a dictionary.
+    //
+    template<size_t N, typename C = char, bool bit_rev = true>
+    static std::basic_string<C> base_n( const void* data, size_t length, const dictionary<N>& dictionary )
+    {
+        // Calculate the group count and reserve the output.
+        //
+        std::basic_string<C> result;
+        size_t group_count = ( length + dictionary.group_size_in() - 1 ) / dictionary.group_size_in();
+        result.resize( group_count * dictionary.group_size_out() );
+        C* rit = result.data();
+
+        // Write each group:
+        //
+        const uint8_t* beg = ( const uint8_t* ) data;
+        const uint8_t* end = beg + length;
+        for ( auto it = beg; it < end; it += dictionary.group_size_in() )
+        {
+            // For each output:
+            //
+            for ( size_t offset = 0; offset != ( dictionary.group_size_in() * 8 ); offset += dictionary.bits_per_char() )
+            {
+                // Insert filler if out of boundaries.
+                //
+                auto in = it + ( offset / 8 );
+                if ( in >= end )
+                {
+                    *rit++ = dictionary.fill();
+                }
+                // Otherwise read the value and encode.
+                //
+                else
+                {
+                    if constexpr ( bit_rev )
+                    {
+                        uint16_t value = uint16_t( math::bit_reverse( in[ 0 ] ) );
+                        if ( ( in + 1 ) != end )
+                            value |= uint16_t( math::bit_reverse( in[ 1 ] ) ) << 8;
+                        value >>= offset % 8;
+                        value = math::bit_reverse<uint8_t>( value & dictionary.mask() ) >> ( 8 - dictionary.bits_per_char() );
+                        *rit++ = dictionary.encode( value );
+                    }
+                    else
+                    {
+                        uint16_t value = uint16_t( in[ 0 ] );
+                        if ( ( in + 1 ) != end )
+                            value |= uint16_t( in[ 1 ] ) << 8;
+                        value >>= offset % 8;
+                        value &= dictionary.mask();
+                        *rit++ = dictionary.encode( value );
+                    }
+                }
+            }
+        }
+        result.resize( rit - result.data() );
+        return result;
+    }
+
+    // Dictionary defined for base64.
+    //
+    static constexpr dictionary base64_dictionary = {
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789"
+        "+/="
+    };
+};
