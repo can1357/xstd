a3a09b08a40f779af37ca3fefa43a99d2ec00793
Can Bölük <can.boluk89@gmail.com>
2021-02-21 04:36:39 +0100

    Minor changes to chores.

--- includes/xstd/chore.hpp
+++ includes/xstd/chore.hpp
@@ -16,41 +16,37 @@ namespace xstd
 	// Registers a chore that will be invoked either at a set time, or if left empty immediately but in an async context.
 	//
 	template<typename T> requires Invocable<T, void>
-	inline void chore( T&& fn, timestamp due_time = {}, int64_t priority = -1 )
+	inline void chore( T&& fn, timestamp due_time, int64_t priority = -1 )
 	{
 #ifdef XSTD_CHORE_SCHEDULER
 		auto [func, arg, _] = flatten( std::forward<T>( fn ) );
-		if ( due_time == timestamp{} )
+		auto [ffunc, farg, __] = flatten( [ due_time ] ()
 		{
-			XSTD_CHORE_SCHEDULER( [ ] ( void* ) { return true; }, nullptr, func, arg, priority );
-		}
-		else
-		{
-			auto [ffunc, farg, __] = flatten( [ due_time ] ()
-			{
-				return time::now() >= due_time;
-			} );
-			XSTD_CHORE_SCHEDULER( ffunc, farg, func, arg, priority );
-		}
+			return time::now() >= due_time;
+		} );
+		XSTD_CHORE_SCHEDULER( ffunc, farg, func, arg, priority );
 #else
-		if ( due_time == timestamp{} )
+		std::thread( [ fn = std::forward<T>( fn ), due_time ]()
 		{
-			std::thread( std::forward<T>( fn ) ).detach();
-		}
-		else
-		{
-			std::thread( [ fn = std::forward<T>( fn ), due_time ]()
-			{
-				std::this_thread::sleep_until( due_time );
-				fn();
-			} ).detach();
-		}
+			std::this_thread::sleep_until( due_time );
+			fn();
+		} ).detach();
 #endif
 	}
 	template<typename T> requires Invocable<T, void>
 	inline void chore( T&& fn, duration delay, int64_t priority = -1 )
 	{
-		return chore( std::forward<T>( fn ), time::now() + delay );
+		return chore( std::forward<T>( fn ), time::now() + delay, priority );
+	}
+	template<typename T> requires Invocable<T, void>
+	inline void chore( T&& fn, int64_t priority = -1 )
+	{
+#ifdef XSTD_CHORE_SCHEDULER
+		auto [func, arg, _] = flatten( std::forward<T>( fn ) );
+		XSTD_CHORE_SCHEDULER( [ ] ( void* ) { return true; }, nullptr, func, arg, priority );
+#else
+		std::thread( std::forward<T>( fn ) ).detach();
+#endif
 	}
 
 	// Registers a chore to be invoked when an even occurs.
