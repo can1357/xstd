352a57bcb78b41f0addaba550d828c819948df66
Can Bölük <can.boluk89@gmail.com>
2021-04-16 03:34:16 +0200

    Improved the logic for GZIP buffer reserving.

--- includes/xstd/gzip.hpp
+++ includes/xstd/gzip.hpp
@@ -7,12 +7,18 @@
 #include "type_helpers.hpp"
 #include "narrow_cast.hpp"
 
+// [[Configuration]]
+// XSTD_GZIP_DEFAULT_LEVEL: Sets the default ZLIB compression level.
+//
+#ifndef XSTD_GZIP_DEFAULT_LEVEL
+	#define XSTD_GZIP_DEFAULT_LEVEL Z_BEST_COMPRESSION
+#endif
+
 namespace xstd::gzip
 {
 	// Compression wrapper.
 	//
-	inline static string_result<std::vector<uint8_t>> compress( const void* data, size_t len,
-												                int level = Z_DEFAULT_COMPRESSION, int strategy = Z_DEFAULT_STRATEGY )
+	inline static string_result<std::vector<uint8_t>> compress( const void* data, size_t len, int level = XSTD_GZIP_DEFAULT_LEVEL, int strategy = Z_DEFAULT_STRATEGY )
 	{
 		z_stream stream;
 		memset( &stream, 0, sizeof( z_stream ) );
@@ -24,7 +30,7 @@ namespace xstd::gzip
 		std::vector<uint8_t> result = {};
 		do
 		{
-			size_t extension_space = 256 + result.size() / 2;
+			size_t extension_space = result.empty() ? len : 256 + result.size() / 2;
 			result.resize( result.size() + extension_space );
 			stream.avail_out = narrow_cast<uInt>( extension_space );
 			stream.next_out = result.data() + result.size() - extension_space;
@@ -45,7 +51,7 @@ namespace xstd::gzip
 	}
 	
 	template<Iterable T> requires is_contiguous_iterable_v<T>
-	inline static string_result<std::vector<uint8_t>> compress( T&& cont, int level = Z_BEST_COMPRESSION, int strategy = Z_DEFAULT_STRATEGY )
+	inline static string_result<std::vector<uint8_t>> compress( T&& cont, int level = XSTD_GZIP_DEFAULT_LEVEL, int strategy = Z_DEFAULT_STRATEGY )
 	{
 		return compress( &*std::begin( cont ), std::size( cont ) * sizeof( iterator_value_type_t<T> ), level, strategy );
 	}
@@ -64,7 +70,7 @@ namespace xstd::gzip
 		std::vector<uint8_t> result = {};
 		do
 		{
-			size_t extension_space = 256 + result.size() * 2;
+			size_t extension_space = result.empty() ? len * 2 : 256 + result.size() * 2;
 			result.resize( result.size() + extension_space );
 			stream.avail_out = narrow_cast<uInt>( extension_space );
 			stream.next_out = result.data() + result.size() - extension_space;
