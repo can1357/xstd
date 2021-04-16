93dd31bd02685a5761583ea22d9acf85877a9cb6
Can Bölük <can.boluk89@gmail.com>
2021-04-17 01:54:06 +0200

    More constexpr.

--- includes/xstd/hexdump.hpp
+++ includes/xstd/hexdump.hpp
@@ -43,6 +43,15 @@ namespace xstd::fmt
 			print_hex_digit( p + n * 2, i[ n ] );
 		return out;
 	}
+	template<Trivial T, typename C = char>
+	inline static constexpr std::array<C, 2*sizeof(T)> cexpr_hex_dump( const T& value )
+	{
+		std::array<C, sizeof( T ) * 2> result = {};
+		auto bytes = xstd::bit_cast<std::array<uint8_t, sizeof(T)>>( value );
+		for ( size_t n = 0; n != sizeof( T ); n++ )
+			xstd::fmt::print_hex_digit( &result[ n * 2 ], bytes[ n ] );
+		return result;
+	}
 
 	// Returns the hexdump of the given range according to the configuration.
 	//
