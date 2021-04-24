b1b9605cd955b9e940bec95f0adbb41964001d91
Can Bölük <can.boluk89@gmail.com>
2021-04-24 05:48:46 +0200

    Shortened ia32::perfmon names, including the namespace itself.

--- includes/ia32/perfmon.hpp
+++ includes/ia32/perfmon.hpp
@@ -1,13 +1,14 @@
 #pragma once
-#include "../ia32.hpp"
 #include <stdint.h>
 #include <string>
+#include <tuple>
 #include <xstd/bitwise.hpp>
+#include "../ia32.hpp"
 
 // This namespace implements partial support for the performance monitoring extensions 
 // implemented by AMD and Intel and tries to unify the interface exposed.
 //
-namespace ia32::perfmon
+namespace ia32::pmu
 {
 	// Common event identifiers that are automatically resolved based on the platform.
 	//
@@ -43,14 +44,13 @@ namespace ia32::perfmon
 
 	// Event selector description.
 	//
-	struct event_selector
+	struct evtsel_t
 	{
 		uint32_t event_select = 0;
 		uint32_t unit_mask = 0;
 		uint32_t count_mask = 0;
 		bool invert = false;
 		bool edge = false;
-
 		inline explicit operator bool() const { return event_select != 0; }
 	};
 
@@ -68,52 +68,52 @@ namespace ia32::perfmon
 	//  - Maps the platform and event identifier to a event selector where possible, otherwise maps to the null selector.
 	//
 	template<bool is_intel, event_id evt>
-	struct dynamic_selector { static constexpr event_selector value = {}; };
+	struct dynamic_selector { static constexpr evtsel_t value = {}; };
 	// -- Common events.
-	template<bool is_intel> struct dynamic_selector<is_intel, event_id::none> { static constexpr event_selector value = {}; };
-	template<bool is_intel> struct dynamic_selector<is_intel, event_id::ins_retire> { static constexpr event_selector value = { 0xC0, 0x00 }; };
-	template<> struct dynamic_selector<true, event_id::br_retire> { static constexpr event_selector value = { 0xC4, 0x00 }; };
-	template<> struct dynamic_selector<false, event_id::br_retire> { static constexpr event_selector value = { 0xC2, 0x00 }; };
-	template<> struct dynamic_selector<true, event_id::uop_retire> { static constexpr event_selector value = { 0xC2, 0x01 }; };
-	template<> struct dynamic_selector<false, event_id::uop_retire> { static constexpr event_selector value = { 0xC1, 0x00 }; };
-	template<> struct dynamic_selector<true, event_id::clock_core> { static constexpr event_selector value = { 0x3C, 0x00 }; };
-	template<> struct dynamic_selector<false, event_id::clock_core> { static constexpr event_selector value = { 0x76, 0x00 }; };
-	template<> struct dynamic_selector<true, event_id::br_miss_retire> { static constexpr event_selector value = { 0xC5, 0x00 }; };
-	template<> struct dynamic_selector<false, event_id::br_miss_retire> { static constexpr event_selector value = { 0xC3, 0x00 }; };
-	template<> struct dynamic_selector<true, event_id::llc_miss> { static constexpr event_selector value = { 0x2E, 0x4F }; };
-	template<> struct dynamic_selector<false, event_id::llc_miss> { static constexpr event_selector value = { 0x43, 0x5B }; };
+	template<bool is_intel> struct dynamic_selector<is_intel, event_id::none> { static constexpr evtsel_t value = {}; };
+	template<bool is_intel> struct dynamic_selector<is_intel, event_id::ins_retire> { static constexpr evtsel_t value = { 0xC0, 0x00 }; };
+	template<> struct dynamic_selector<true, event_id::br_retire> { static constexpr evtsel_t value = { 0xC4, 0x00 }; };
+	template<> struct dynamic_selector<false, event_id::br_retire> { static constexpr evtsel_t value = { 0xC2, 0x00 }; };
+	template<> struct dynamic_selector<true, event_id::uop_retire> { static constexpr evtsel_t value = { 0xC2, 0x01 }; };
+	template<> struct dynamic_selector<false, event_id::uop_retire> { static constexpr evtsel_t value = { 0xC1, 0x00 }; };
+	template<> struct dynamic_selector<true, event_id::clock_core> { static constexpr evtsel_t value = { 0x3C, 0x00 }; };
+	template<> struct dynamic_selector<false, event_id::clock_core> { static constexpr evtsel_t value = { 0x76, 0x00 }; };
+	template<> struct dynamic_selector<true, event_id::br_miss_retire> { static constexpr evtsel_t value = { 0xC5, 0x00 }; };
+	template<> struct dynamic_selector<false, event_id::br_miss_retire> { static constexpr evtsel_t value = { 0xC3, 0x00 }; };
+	template<> struct dynamic_selector<true, event_id::llc_miss> { static constexpr evtsel_t value = { 0x2E, 0x4F }; };
+	template<> struct dynamic_selector<false, event_id::llc_miss> { static constexpr evtsel_t value = { 0x43, 0x5B }; };
 	// -- Intel exclusive events.
-	template<> struct dynamic_selector<true, event_id::ins_execute> { static constexpr event_selector value = { 0x16, 0x00 }; };
-	template<> struct dynamic_selector<true, event_id::uop_execute> { static constexpr event_selector value = { 0xB1, 0x00 }; };
-	template<> struct dynamic_selector<true, event_id::uop_dispatch> { static constexpr event_selector value = { 0xA1, 0xFF }; };
-	template<> struct dynamic_selector<true, event_id::hw_interrupt_receive> { static constexpr event_selector value = { 0xCB, 0x01 }; };
-	template<> struct dynamic_selector<true, event_id::hw_interrupt_masked> { static constexpr event_selector value = { 0xCB, 0x02 }; };
-	template<> struct dynamic_selector<true, event_id::hw_interrupt_pending_masked> { static constexpr event_selector value = { 0xCB, 0x04 }; };
+	template<> struct dynamic_selector<true, event_id::ins_execute> { static constexpr evtsel_t value = { 0x16, 0x00 }; };
+	template<> struct dynamic_selector<true, event_id::uop_execute> { static constexpr evtsel_t value = { 0xB1, 0x00 }; };
+	template<> struct dynamic_selector<true, event_id::uop_dispatch> { static constexpr evtsel_t value = { 0xA1, 0xFF }; };
+	template<> struct dynamic_selector<true, event_id::hw_interrupt_receive> { static constexpr evtsel_t value = { 0xCB, 0x01 }; };
+	template<> struct dynamic_selector<true, event_id::hw_interrupt_masked> { static constexpr evtsel_t value = { 0xCB, 0x02 }; };
+	template<> struct dynamic_selector<true, event_id::hw_interrupt_pending_masked> { static constexpr evtsel_t value = { 0xCB, 0x04 }; };
 	// -- AMD exclusive events.
-	template<> struct dynamic_selector<false, event_id::smi_received> { static constexpr event_selector value = { 0x2B, 0x00 }; };
+	template<> struct dynamic_selector<false, event_id::smi_received> { static constexpr evtsel_t value = { 0x2B, 0x00 }; };
 
 	// Shortcuts.
 	//
-	template<bool is_intel, event_id evt> inline constexpr size_t fixed_counter_v = fixed_counter<is_intel, evt>::value;
-	template<bool is_intel, event_id evt> inline constexpr event_selector dynamic_selector_v = dynamic_selector<is_intel, evt>::value;
+	template<bool is_intel, event_id evt> inline constexpr size_t   fixed_counter_v =    fixed_counter<is_intel, evt>::value;
+	template<bool is_intel, event_id evt> inline constexpr evtsel_t dynamic_selector_v = dynamic_selector<is_intel, evt>::value;
 
 	// Configuration traits for each platform.
 	//
 	template<bool is_intel>
-	struct config_traits
+	struct unit
 	{
 		// Counter MSRs.
 		//
-		static constexpr uint32_t config_base = is_intel ? IA32_PERFEVTSEL0 : 0xC0010000;
-		static constexpr uint32_t counter_base = is_intel ? IA32_PMC0 : 0xC0010004;
+		static constexpr uint32_t config_base = is_intel ? IA32_PERFEVTSEL0 : IA32_PERFEVTSEL0_AMD;
+		static constexpr uint32_t counter_base = is_intel ? IA32_PMC0 : IA32_PMC0_AMD;
 		static constexpr uint32_t counter_limit = is_intel ? 8 : 4;
 		static constexpr uint32_t counter_stride = 1;
 
 		// Aliasing counter MSRs.
 		// - Intel uses these to extend the numeric range, AMD uses these to implement extension counters.
 		//
-		static constexpr uint32_t aliasing_config_base = is_intel ? IA32_PERFEVTSEL0 : 0xC0010200;
-		static constexpr uint32_t aliasing_counter_base = is_intel ? IA32_A_PMC0 : 0xC0010201;
+		static constexpr uint32_t aliasing_config_base = is_intel ? IA32_PERFEVTSEL0 : IA32_A_PERFEVTSEL0_AMD;
+		static constexpr uint32_t aliasing_counter_base = is_intel ? IA32_A_PMC0 : IA32_A_PMC0_AMD;
 		static constexpr uint32_t aliasing_counter_limit = is_intel ? 8 : 6;
 		static constexpr uint32_t aliasing_counter_stride = is_intel ? 1 : 2;
 
@@ -204,30 +204,32 @@ namespace ia32::perfmon
 			return { fixed_control, fixed_counter_base + offset };
 		}
 	};
+	using intel = unit<true>;
+	using amd =   unit<false>;
 
 	// Counter flags.
 	// - Maps to perfevtsel bits 1:1 for convenience.
 	//
 	enum counter_flags : uint32_t
 	{
-		ctr_enable = IA32_PERFEVTSEL_EN_FLAG,
-		ctr_user = IA32_PERFEVTSEL_USR_FLAG,
+		ctr_enable =     IA32_PERFEVTSEL_EN_FLAG,
+		ctr_user =       IA32_PERFEVTSEL_USR_FLAG,
 		ctr_supervisor = IA32_PERFEVTSEL_OS_FLAG,
 		ctr_any_thread = IA32_PERFEVTSEL_ANY_THREAD_FLAG,
-		ctr_interrupt = IA32_PERFEVTSEL_INTR_FLAG,
+		ctr_interrupt =  IA32_PERFEVTSEL_INTR_FLAG,
 	};
 
 	// Changes the state configuration given the dynamic/fixed counter. 
 	// - Returns false/npos on failure, else true/counter index.
 	//
-	FORCE_INLINE inline bool dynamic_set_state( size_t index, event_selector selector, uint32_t flags, bool update_global = false )
+	FORCE_INLINE inline bool dynamic_set_state( size_t index, evtsel_t selector, uint32_t flags, bool update_global = false )
 	{
 		// Visit the platform.
 		//
 		bool success = false;
 		xstd::visit_range<false, true>( is_intel(), [ & ] <bool is_intel> ( xstd::const_tag<is_intel> )
 		{
-			using traits = config_traits<is_intel>;
+			using traits = unit<is_intel>;
 
 			// Resolve the counter.
 			//
@@ -272,7 +274,7 @@ namespace ia32::perfmon
 			{
 				// Map and validate the selector.
 				//
-				event_selector selector = dynamic_selector_v<is_intel, evt>;
+				evtsel_t selector = dynamic_selector_v<is_intel, evt>;
 				if ( !selector && evt != event_id::none )
 					return;
 				success = dynamic_set_state( index, selector, flags, update_global );
@@ -287,7 +289,7 @@ namespace ia32::perfmon
 		size_t index = std::string::npos;
 		xstd::visit_range<false, true>( is_intel(), [ & ] <bool is_intel> ( xstd::const_tag<is_intel> )
 		{
-			using traits = config_traits<is_intel>;
+			using traits = unit<is_intel>;
 
 			// Validate the fixed control register.
 			//
@@ -349,7 +351,7 @@ namespace ia32::perfmon
 		//
 		return *xstd::visit_range<false, true>( is_intel(), [ & ] <bool is_intel> ( xstd::const_tag<is_intel> ) -> counter_flags
 		{
-			using traits = config_traits<is_intel>;
+			using traits = unit<is_intel>;
 
 			// Returns zero (disabled) if the dynamic counter index is invalid.
 			//
@@ -377,7 +379,7 @@ namespace ia32::perfmon
 	{
 		return *xstd::visit_range<false, true>( is_intel(), [ & ] <bool is_intel> ( xstd::const_tag<is_intel> ) -> counter_flags
 		{
-			using traits = config_traits<is_intel>;
+			using traits = unit<is_intel>;
 
 			// Returns zero (disabled) if the fixed counter index is invalid.
 			//
@@ -422,7 +424,7 @@ namespace ia32::perfmon
 		//
 		return *xstd::visit_range<false, true>( is_intel(), [ & ] <bool is_intel> ( xstd::const_tag<is_intel> ) -> bool
 		{
-			using traits = config_traits<is_intel>;
+			using traits = unit<is_intel>;
 
 			// Return false if the dynamic counter index is invalid.
 			//
@@ -442,7 +444,7 @@ namespace ia32::perfmon
 		//
 		return *xstd::visit_range<false, true>( is_intel(), [ & ] <bool is_intel> ( xstd::const_tag<is_intel> ) -> bool
 		{
-			using traits = config_traits<is_intel>;
+			using traits = unit<is_intel>;
 
 			// Return false if the fixed counter index is invalid.
 			//
@@ -465,7 +467,7 @@ namespace ia32::perfmon
 	{
 		return *xstd::visit_range<false, true>( is_intel(), [ & ] <bool is_intel> ( xstd::const_tag<is_intel> ) -> uint64_t
 		{
-			using traits = config_traits<is_intel>;
+			using traits = unit<is_intel>;
 			auto [cfg, cnt] = traits::resolve_dynamic_counter( index );
 			return ( cnt && cfg ) ? read_msr( cnt ) : 0;
 		} );
@@ -474,7 +476,7 @@ namespace ia32::perfmon
 	{
 		return *xstd::visit_range<false, true>( is_intel(), [ & ] <bool is_intel> ( xstd::const_tag<is_intel> ) -> uint64_t
 		{
-			using traits = config_traits<is_intel>;
+			using traits = unit<is_intel>;
 			auto [cfg, cnt] = traits::resolve_fixed_counter( index );
 			return ( cnt && cfg ) ? read_msr( cnt ) : 0;
 		} );
