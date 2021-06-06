4d46e0ba5fda8979241497a24c7653f8c2aec3fd
Can Bölük <can.boluk89@gmail.com>
2021-06-06 06:52:56 +0200

    Improved narrow cast logic.

--- includes/xstd/narrow_cast.hpp
+++ includes/xstd/narrow_cast.hpp
@@ -8,33 +8,28 @@
 
 namespace xstd
 {
-	// Checks whether or not a value can be clamped to a given type.
+	// Checks whether or not a value can be narrowed down to a given type.
 	//
-	template<Integral T, Integral T2>
-	__forceinline static constexpr bool range_check( T2 o )
+	template<Integral Dst, Integral Src>
+	__forceinline static constexpr bool within_limits( Src o )
 	{
-		// If signs mismatch:
-		//
-		if constexpr ( std::is_signed_v<T2> ^ std::is_signed_v<T> )
-			return 0 <= o && size_t( o ) <= size_t( std::numeric_limits<T>::max() );
-		else
-			return std::numeric_limits<T>::min() <= o && o <= std::numeric_limits<T>::max();
+		return ( ( Dst ) o ) == o;
 	}
 
 	// Narrows the given type in a safe manner.
 	//
-	template<Integral T, Integral T2> requires( sizeof( T ) <= sizeof( T2 ) || std::is_signed_v<T> != std::is_signed_v<T2> )
-	__forceinline static constexpr T narrow_cast( T2 o )
+	template<Integral Dst, Integral Src>
+	__forceinline static constexpr Dst narrow_cast( Src o )
 	{
-		dassert( range_check<T>( o ) );
-		return ( T ) o;
+		dassert( within_limits<Dst>( o ) );
+		return ( Dst ) o;
 	}
-	template<Integral T, Integral T2> requires( sizeof( T ) <= sizeof( T2 ) || std::is_signed_v<T> != std::is_signed_v<T2> )
-	__forceinline static constexpr std::optional<T> narrow_cast_s( T2 o )
+	template<Integral Dst, Integral Src>
+	__forceinline static constexpr std::optional<Dst> narrow_cast_s( Src o )
 	{
-		std::optional<T> result = {};
-		if ( range_check<T>( o ) )
-			result.emplace( ( T ) o );
+		std::optional<Dst> result = {};
+		if ( within_limits<Dst>( o ) )
+			result.emplace( ( Dst ) o );
 		return result;
 	}
 };
