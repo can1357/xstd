ccc492f7c36ba1edd8b1de9663df73b9eebaa202
Can Bölük <can.boluk89@gmail.com>
2021-06-09 22:39:23 +0200

    Implemented narrow cast for arbitrary bit size for bitfields.

--- includes/xstd/narrow_cast.hpp
+++ includes/xstd/narrow_cast.hpp
@@ -15,6 +15,35 @@ namespace xstd
 	{
 		return ( ( Dst ) o ) == o;
 	}
+	template<Integral Dst, Integral Src>
+	__forceinline static constexpr bool narrow_viable( Src o, int bits /*for bitfields.*/ )
+	{
+		bits = std::min( bits, sizeof( Dst ) * 8 );
+
+		// Validate sign.
+		//
+		if constexpr ( std::is_signed_v<Src> && !std::is_signed_v<Dst> )
+		{
+			if ( o < 0 )
+				return false;
+		}
+		
+		// If signed, [-2^(x-1), (2^(x-1))-1]:
+		//
+		if constexpr ( std::is_signed_v<Dst> )
+		{
+			const int64_t signed_max = ( 1ll << ( bits - 1 ) ) - 1;
+			const int64_t signed_min = -( 1ll << ( bits - 1 ) );
+			return signed_min <= o && o <= signed_max;
+		}
+		// If unsigned, [0, 2^x-1]:
+		//
+		else
+		{
+			const uint64_t unsigned_max = ( 1ull << bits ) - 1;
+			return o <= unsigned_max;
+		}
+	}
 
 	// Narrows the given type in a safe manner.
 	//
