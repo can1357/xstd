56caed4fd7dfda4836fb1aca451ac59f5b904a66
can1357 <me@can.ac>
2023-10-17 21:13:10 +0300

    Allow type-decay if it has virtual destructor.

--- includes/xstd/ref_counted.hpp
+++ includes/xstd/ref_counted.hpp
@@ -20,7 +20,7 @@ namespace xstd
 
 		// Construction by pointer.
 		//
-		explicit ref( store_type* ptr, bool add_ref = true ) : ptr( ptr )
+		constexpr explicit ref( store_type* ptr, bool add_ref = true ) : ptr( ptr )
 		{ 
 			if ( ptr && add_ref ) 
 				ptr->ref_count++; 
@@ -28,59 +28,59 @@ namespace xstd
 
 		// Null reference.
 		//
-		ref() : ptr( nullptr ) {}
-		ref( std::nullptr_t ) : ptr( nullptr ) {}
+		constexpr ref() = default;
+		constexpr ref( std::nullptr_t ) : ptr( nullptr ) {}
 
 		// Move and copy.
 		//
-		ref( ref&& o ) noexcept : ptr( std::exchange( o.ptr, nullptr ) ) {}
-		ref( const ref& o ) : ref( o.ptr, true ) {}
-		ref& operator=( ref&& o ) noexcept 
-		{ 
-			std::swap( ptr, o.ptr ); 
-			return *this; 
+		constexpr ref( ref&& o ) noexcept : ptr( std::exchange( o.ptr, nullptr ) ) {}
+		constexpr ref( const ref& o ) : ref( o.ptr, true ) {}
+		constexpr ref& operator=( ref&& o ) noexcept {
+			std::swap( ptr, o.ptr );
+			return *this;
 		}
-		ref& operator=( const ref& o ) 
-		{ 
-			reset( o.ptr ); 
-			return *this; 
+		constexpr ref& operator=( const ref& o ) {
+			reset( o.ptr );
+			return *this;
 		}
+
+		// Type cast.
+		//
+		template<typename Ty> requires ( std::is_base_of_v<T, Ty> && std::has_virtual_destructor_v<T> )
+		constexpr ref( const ref<Ty>& o ) : ref( o.ptr, true ) {}
+		template<typename Ty> requires ( std::is_base_of_v<T, Ty> && std::has_virtual_destructor_v<T> )
+		constexpr ref( ref<Ty>&& o ) : ref( o.release(), false ) {}
 
 		// Reset and release.
 		//
-		void reset( store_type* new_ptr = nullptr )
-		{
+		constexpr void reset( store_type* new_ptr = nullptr ) {
 			if ( new_ptr )
 				new_ptr->ref_count++;
-			if ( auto p = std::exchange( ptr, new_ptr ) )
-			{
+			if ( auto p = std::exchange( ptr, new_ptr ) ) {
 				if ( !--p->ref_count ) [[unlikely]]
 					delete p;
 			}
 		}
-		store_type* release() { return std::exchange( ptr, nullptr ); }
+		constexpr store_type* release() { return std::exchange( ptr, nullptr ); }
 
 		// Validity check and pointer like semantics.
 		//
-		explicit operator bool() const { return ptr != nullptr; }
-		T* operator->() const { return ptr; }
-		T& operator*() const { return *ptr; }
-		T* get() const { return ptr; }
+		constexpr explicit operator bool() const { return ptr != nullptr; }
+		constexpr T* operator->() const { return ptr; }
+		constexpr T& operator*() const { return *ptr; }
+		constexpr T* get() const { return ptr; }
 
 		// Reference helpers.
 		//
-		size_t inc_ref() const 
-		{ 
+		size_t inc_ref() const { 
 			return ++ptr->ref_count; 
 		}
-		size_t dec_ref() const
-		{ 
+		size_t dec_ref() const { 
 			size_t n = --ptr->ref_count;
 			fassert( n != 0 );
 			return n;
 		}
-		size_t dec_ref()
-		{
+		size_t dec_ref() {
 			size_t n = --ptr->ref_count;
 			if ( !n )
 				delete std::exchange( ptr, nullptr );
@@ -90,7 +90,7 @@ namespace xstd
 
 		// Dec-ref on destroy.
 		//
-		~ref() { reset(); }
+		constexpr ~ref() { reset(); }
 	};
 
 	// enable_shared_from_this equivalent.
@@ -99,13 +99,11 @@ namespace xstd
 	struct ref_counted : ref_counted_base
 	{
 		template<typename Ty = T>
-		ref<Ty> add_ref()
-		{  
+		ref<Ty> add_ref() {  
 			return ref<Ty>{ static_cast<Ty*>( this ) };
 		}
 		template<typename Ty = T>
-		ref<const Ty> add_ref() const
-		{ 
+		ref<const Ty> add_ref() const {
 			return ref<const Ty>{ static_cast<const Ty*>( this ) };
 		}
 	};
@@ -113,8 +111,7 @@ namespace xstd
 	// Creates a ref-counted object.
 	//
 	template<typename T, typename... Tx> 
-	inline ref<T> make_refc( Tx&&... args ) 
-	{ 
+	inline ref<T> make_refc( Tx&&... args ) { 
 		using V = typename ref<T>::store_type;
 		return ref<T>( new V( std::forward<Tx>( args )... ), false );
 	}
