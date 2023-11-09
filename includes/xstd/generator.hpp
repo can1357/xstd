5a85c99771a1b5382231e4825810e520f8c73f24
can1357 <me@can.ac>
2023-11-09 07:33:02 +0300

    Generators.

--- includes/xstd/generator.hpp
+++ includes/xstd/generator.hpp
@@ -1,90 +1,65 @@
 #pragma once
-#include <optional>
 #include "coro.hpp"
+#include <optional>
 
-namespace xstd
-{
+namespace xstd {
 	// Simple generator coroutine.
 	//
 	template<typename T>
-	struct generator
-	{
-		struct promise_type
-		{
-			std::optional<T> current = std::nullopt;
+	struct generator {
+		struct promise_type {
 			coroutine_handle<> continuation = {};
+			std::optional<T>*  placement;
 
-			struct yield_awaitable
-			{
-				inline bool await_ready() noexcept { return false; }
-
-				template<typename P = promise_type>
-				inline coroutine_handle<> await_suspend( coroutine_handle<P> handle ) noexcept
-				{
-					if ( auto c = std::exchange( handle.promise().continuation, nullptr ) )
-						return c;
-					else
-						return noop_coroutine();
+			struct yield_awaitable {
+				inline bool await_ready() { return false; }
+				inline coroutine_handle<> await_suspend( coroutine_handle<promise_type> handle ) noexcept {
+					return handle.promise().continuation;
 				}
-				inline void await_resume() noexcept {}
+				inline void await_resume() {}
 			};
 			template<typename V>
-			yield_awaitable yield_value( V&& value )
-			{
-				current.emplace( std::forward<V>( value ) );
+			yield_awaitable yield_value( V&& value ) {
+				std::exchange( placement, nullptr )->emplace( std::forward<V>( value ) );
 				return {};
 			}
 			generator get_return_object() { return *this; }
 			suspend_always initial_suspend() { return {}; }
-			yield_awaitable final_suspend() noexcept { return {}; }
+			suspend_always final_suspend() noexcept {}
 			void return_void() {}
 			XSTDC_UNHANDLED_RETHROW;
 		};
 
-		struct iterator
-		{
-			coroutine_handle<promise_type> handle = {};
-			
-			iterator& operator++()
-			{
-				handle.resume();
-				if ( handle.done() )
-					handle = nullptr;
-				return *this;
-			}
-			bool operator==( const iterator& rhs ) const { return handle == rhs.handle; }
-			bool operator!=( const iterator& rhs ) const { return handle != rhs.handle; }
-			T&& operator*() const { return *std::move( handle.promise().current ); }
-		};
-		iterator begin() { return finished() ? end() : ++iterator{ handle }; }
-		iterator end() const { return {}; }
-
+		// Returns whether or not the generator is done producing values.
+		//
 		bool finished() const { return handle.done(); }
-		std::optional<T> operator()()
-		{ 
-			if ( finished() )
-				return std::nullopt;
-			handle.resume(); 
-			return std::move( handle.promise().current ); 
-		}
+		explicit operator bool() const { return !finished; }
 
+		// Returns the work that needs to be started for the generator to start producing values.
+		//
+		bool work_released = false;
+		coroutine_handle<> worker() { return std::exchange( work_released, true ) ? nullptr : handle; }
+
+		// Internal constructor.
+		//
 		unique_coroutine<promise_type> handle;
 		generator( promise_type& pr ) : handle( pr ) {}
 
-		// Make awaitable for symetric generators.
+		// Implement await.
 		//
-		inline bool await_ready() { return finished(); }
-		inline coroutine_handle<> await_suspend( coroutine_handle<> h )
-		{ 
-			handle.promise().continuation = h;
-			return handle.get();
-		}
-		inline std::optional<T> await_resume()
-		{
-			if ( finished() )
-				return std::nullopt;
-			else
-				return std::move( handle.promise().current );
-		}
+		struct awaiter {
+			coroutine_handle<promise_type> hnd;
+			std::optional<T> result = std::nullopt;
+
+			inline bool await_ready() { return hnd.done(); }
+			inline coroutine_handle<> await_suspend( coroutine_handle<> h ) {
+				auto& promise = hnd.promise();
+				promise.placement =    &result;
+				promise.continuation = h;
+				return hnd;
+			}
+			inline std::optional<T>&& await_resume() { return std::move( result ); }
+		};
+		inline awaiter operator co_await() { return { handle.hnd }; }
 	};
 };
